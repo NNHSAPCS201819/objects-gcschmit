9d3e3f5a09426ed2ea25b14bf1bbe07ed0b5eef7
Geoff Schmit <gcschmit@gmail.com>
2018-08-21 09:33:24 -0500

    atarted the HelloPrinter class

---
+++ ClassNotes/HelloPrinter.java
@@ -0,0 +1,25 @@
+
+/**
+ * Every Java program contains one or more classes (e.g., HelloPrinter)
+ * 
+ * In general, every source file (e.g., HelloPrinter.java) contains one
+ *      class (e.g., HelloPrinter).
+ *      
+ * The source file name must match the class name.
+ * 
+ * By convention, class names start with an uppercase letter.
+ *
+ * @author gcschmit
+ * @version 21 August 2018
+ */
+public class HelloPrinter
+{
+    /*
+     * A class contains methods.
+     * 
+     * Most Java applications contain a class with a main method.
+     * 
+     * The main method is executed when the application starts.
+     */
+    
+}
