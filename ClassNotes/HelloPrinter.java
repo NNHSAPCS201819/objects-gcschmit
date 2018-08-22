6256435b43de2f5743d9837f439de832e4c6fc1e
Geoff Schmit <gcschmit@gmail.com>
2018-08-22 10:29:02 -0500

    added identifier examples

--- ClassNotes/HelloPrinter.java
+++ ClassNotes/HelloPrinter.java
@@ -21,5 +21,55 @@ public class HelloPrinter
      * 
      * The main method is executed when the application starts.
      */
-    
+    public static void main( String[] args )
+    {
+        /*
+         * A method contains statements.
+         * 
+         * Statements may invoke other methods (e.g., println).
+         * 
+         * In general, methods are invoked on objects (e.g., System.out).
+         * 
+         * When invoking a method, arguments are passed in parentheses
+         *      (e.g., "Hello, World!").
+         */
+        System.out.println( "Hello, World!" ); // comment at the end of the line
+        
+        // this is a single-line comment (like # in python)
+        
+        /*
+         * Identifiers are any combination of:
+         *      letters
+         *      digits (but not as a first character)
+         *      underscores (_)
+         *      dollar signs($)
+         * 
+         *  These are various identifiers:
+         */
+        int y, x2, x_y, x$;
+        
+        // This is not a valid identifier:
+        //int 2y;
+        
+        /*
+         * Identifiers are case sensitive (case matters).
+         * 
+         * These are three different identifiers:
+         */
+        String box, BOX, Box;
+        
+        /*
+         * By convention, constants are all uppercase.
+         *      Use an underscore to separate words.
+         */
+        int SPEED_OF_LIGHT = 300000000;
+        
+        /*
+         * By convention, all other identifiers start with a lowercase letter.
+         *      Subsequent words start with an uppercase letter (CamalCase)
+         */
+        String firstName;
+    }
 }
+
+
